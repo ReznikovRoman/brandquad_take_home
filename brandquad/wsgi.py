d3bb6348e312a10dc2703e499c18f296654480f0
Roman Reznikov <romanreznikov2002@yandex.ru>
2024-09-02 09:13:41 -0400

    fix: change project name in settings to `brandquad`

--- brandquad/wsgi.py
+++ brandquad/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netflix.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brandquad.settings")
 
 application = get_wsgi_application()
