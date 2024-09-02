e16fd894f3a125db4afedf7a83db6a82d2a29901
Roman Reznikov <romanreznikov2002@yandex.ru>
2024-09-02 09:14:02 -0400

    fix: remove redundant environment variables

--- tools/mypy_plugins/mypy_django_configurations_plugin.py
+++ tools/mypy_plugins/mypy_django_configurations_plugin.py
@@ -4,9 +4,6 @@ from configurations import importer
 from mypy_django_plugin import main
 from mypy_django_plugin.main import NewSemanalDjangoPlugin
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brandquad.settings")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Lint")
-
 
 def plugin(version: str) -> type[NewSemanalDjangoPlugin]:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brandquad.settings")
